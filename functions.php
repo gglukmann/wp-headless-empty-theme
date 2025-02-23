e7a827f75be27577c07c60299b63b3757cefbc5c
Gert Glükmann <gglukmann@gmail.com>
2025-02-23 12:18:16 +0200

    Add empty theme for headless wordpress

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+function remove_menus()
+{
+  // remove_menu_page('edit.php');                   //Posts
+  // remove_menu_page('edit.php?post_type=page');    //Pages
+  remove_menu_page('edit-comments.php');          //Comments
+}
+add_action('admin_menu', 'remove_menus');
